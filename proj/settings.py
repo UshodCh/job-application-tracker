eba3cfc2e9785c653a94cbb62b89d3908dc6f1a5
BhavyaPondugula <pondugula212001@gmail.com>
2026-06-06 20:15:51 +0530

    Fixed CSRF errors

--- proj/settings.py
+++ proj/settings.py
@@ -7,7 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-CSRF_TRUSTED_ORIGINS = ['https://web-production-8fea4.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://job-application-tracker-gcbo.onrender.com',
+    'https://web-production-8fea4.up.railway.app'
+]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
